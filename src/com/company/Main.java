c04fac0749edead4a51d4982f4a07f28ad4dec86
Zigmas <zambrazaitis@gmail.com>
2018-01-31 15:19:00 +0200

    uzduotis3.05

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    private int suma;
+    private int masyvas[];
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int suma = 0;
-        int[] masyvas = new int[5];
-        System.out.println("Iveskite penkis skaicius: ");
+        Main objektas = new Main();
+        objektas.nuskaitymas();
+        objektas.isvedimas();
+    }
 
-        for (int i = 0; i < 5; i++) {
+    public void nuskaitymas() {
+        Scanner sc = new Scanner(System.in);
+        masyvas = new int[5];
+        for (int i = 0; i<masyvas.length; i++) {
+            System.out.println("Iveskite skaiciu nr. " + (i+1) + ": ");
             masyvas[i] = sc.nextInt();
             suma += masyvas[i];
         }
+    }
+
+    public void isvedimas() {
         System.out.println("Suma yra lygi: " + suma);
         System.out.println("Jusu ivesti skaiciai: ");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(masyvas[i]);
+        for (int i = 0; i <masyvas.length; i++) {
+            System.out.println("Jusu " + (i+1) + "-asis ivestas skaicius: " + masyvas[i]);
         }
         // System.out.println("Pasirinkti skaiciai: " + masyvas[0] + ", " + masyvas[1] + ", " + masyvas [2] + ", " + masyvas[3] + ", " + masyvas[4] + ".");
     }
